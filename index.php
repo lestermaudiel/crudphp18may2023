e1acea35385fa5ab8b945fb907bbb9cc311a3fc9
lestermaudiel <flestermaudiel2130@gmail.com>
2023-06-10 17:32:12 -0600

    se realizaron todos los cambios

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+    // require './modelos/Conexion.php';
+    // $resultado = Conexion::servir("SELECT * FROM PRODUCTOS");
+    // echo "<pre>";
+    // var_dump($resultado);
+    // echo "</pre>";
