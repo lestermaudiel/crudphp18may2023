d8e6c22842b3de6afe30a3d714f5afcc670a0a63
lestermaudiel <flestermaudiel2130@gmail.com>
2023-06-10 18:03:50 -0600

    elimar index.

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-    // require './modelos/Conexion.php';
-    // $resultado = Conexion::servir("SELECT * FROM PRODUCTOS");
-    // echo "<pre>";
-    // var_dump($resultado);
-    // echo "</pre>";
